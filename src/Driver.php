924dd7abbe3b3971c2771a0c7ce8b64c7191ed8e
翟帅干 <zhaishuaigan@qq.com>
2016-06-17 18:42:36 +0800

    up

--- src/Driver.php
+++ src/Driver.php
@@ -3,7 +3,7 @@
 // +----------------------------------------------------------------------
 // | OneThink [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2014 http://www.onethink.cn All rights reserved.
+// | Copyright (c) 2016 http://www.thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Author: 翟帅干 <zhaishuaigan@qq.com> <http://zhaishuaigan.cn>
 // +----------------------------------------------------------------------
@@ -11,6 +11,8 @@
 namespace think\view\driver;
 
 use think\Config;
+use think\Request;
+use think\App;
 
 class Angular {
 
@@ -20,25 +22,28 @@ class Angular {
 
     public function __construct($config = []) {
         $this->config   = [
-            'tpl_path'         => VIEW_PATH,
+            'tpl_path'         => App::$modulePath . 'view' . DS,
             'tpl_suffix'       => Config::get('view.engine_config.suffix') ? : '.html',
             'tpl_cache_path'   => CACHE_PATH,
             'tpl_cache_suffix' => Config::get('view.engine_config.cache') ? : '.php',
             'attr'             => 'php-',
         ];
         $this->template = new \Angular($this->config);
-
         // 初始化模板编译存储器
         $storage       = Config::get('compile_type') ? : '\\think\\template\\driver\\File';
         $this->storage = new $storage();
     }
 
     public function fetch($template, $data = [], $cache = []) {
-        $template       = $template ? $template :
-                CONTROLLER_NAME . '/' . ACTION_NAME;
+        if (!$template) {
+            $request    = Request::instance();
+            $controller = $request->controller();
+            $action = $request->action();
+            $template       = $controller. DS . $action;
+        }
         // 根据模版文件名定位缓存文件
         $tpl_cache_file = CACHE_PATH . 'angular_' . md5($template) . '.php';
-        if (APP_DEBUG || !is_file($tpl_cache_file) || !$this->storage->check($tpl_cache_file, 0)) {
+        if (App::$debug || !is_file($tpl_cache_file) || !$this->storage->check($tpl_cache_file, 0)) {
             // 编译模板内容
             $content = $this->template->compiler($template, $data);
             $this->storage->write($tpl_cache_file, $content);
